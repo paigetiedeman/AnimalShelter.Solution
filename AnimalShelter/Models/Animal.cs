dce565b06f313391d8e2706d3a9a7bc6e2a6fcd2
Paige Tiedeman <paigetiedeman@gmail.com>
2021-10-05 16:14:20 -0700

    displaying Animal properties in list
    Co-authored-by: Anna Clarke  <annac.klingberg@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -6,6 +6,9 @@ namespace AnimalShelter.Models
   {
     public int AnimalId { get; set; }
     public string Type { get; set; }
-    
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string Date { get; set; }
+    public string Breed { get; set; }
   }
 }
